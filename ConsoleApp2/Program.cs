7c8c722b590a9e3b8de4fe988f3298efc2ccb988
yeahdardy <averydc@hotmail.com>
2019-10-29 19:17:41 +1100

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,6 +6,8 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
           System.Console.WriteLine("Hello World");  
+          System.Console.WriteLine("This is a placeholder textline where one day a meaningful sentence will be!");
+          System.Console.ReadLine();
         }
     }
 }
